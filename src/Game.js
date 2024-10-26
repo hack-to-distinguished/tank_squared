5678ef82ad8b1312385ae72652be638ec1ed35d1
Alejandro.DGZ <3NJDEGUZMAN@gmail.com>
2024-10-26 19:06:13 +0100

    added bullet firing, basics

--- src/Game.js
+++ src/Game.js
@@ -3,14 +3,70 @@ await app.init({ width: 1280, height: 720 }); // background for the game
 
 // load the PNG asynchronously/ creating the sprite itself
 await PIXI.Assets.load('assets/images/tank.png');
+
+let bullets = [];
+let bulletSpeed = 10;
+
 let playerOne = PIXI.Sprite.from('assets/images/tank.png');
 playerOne.height = 100;
 playerOne.width = 225;
-playerOne.x = width/2;
-playerOne.y = width/2;
+playerOne.y = 300;
 document.body.appendChild(app.canvas);
 app.stage.addChild(playerOne);
 
+// keyboard event handlers
+window.addEventListener("keydown", keysDown);
+
+// app.stage.interactive = true;
+// app.stage.on("pointerdown", fireBullet);
+
+window.addEventListener("pointerdown", fireBullet);
+
+function fireBullet(e) {
+    console.log("FIRE!");    
+
+    bullets.push(createBullet(e));
+    console.log(bullets);
+} 
+
+// Function to update the bullets' positions each frame
+function updateBullets(delta) {
+    for (let i = bullets.length - 1; i >= 0; i--) {
+        bullets[i].position.x -= bullets[i].speed;
+
+        // Remove bullet if it goes off the screen
+        if (bullets[i].position.x < 0) {
+            app.stage.removeChild(bullets[i]);
+            bullets.splice(i, 1);
+        }
+    }
+}
+
+
+function createBullet(e) {
+    let bullet = PIXI.Sprite.from('assets/images/tank.png');
+    bullet.width = 50;
+    bullet.height = 50;
+    bullet.speed = bulletSpeed;
+    bullet.anchor.set(0.5, 0.5);
+    app.stage.addChild(bullet);
+
+    // Position the bullet at the player's position
+    bullet.x = playerOne.x;
+    bullet.y = playerOne.y;
+
+    return bullet;
+
+}
+
+function keysDown(e) {
+    if (e.keyCode == 68) {
+        playerOne.x += 10;
+    } 
+    if (e.keyCode == 65) {
+        playerOne.x -= 10;
+    }
+}
 
 // This is for updating the game environment itself
 // Add a variable to count up the seconds our demo has been running
@@ -20,7 +76,5 @@ let elapsed = 0.0;
 app.ticker.add((ticker) => {
     // Add the time to our total elapsed time
     elapsed += ticker.deltaTime;
-    // Update the sprite's X position based on the cosine of our elapsed time.  We divide
-    // by 50 to slow the animation down a bit...
-    playerOne.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+    updateBullets(ticker);
 });

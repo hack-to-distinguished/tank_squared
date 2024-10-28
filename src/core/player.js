88d569e1646dc4edf2f267f34006406cac605f06
Alejandro.DGZ <3NJDEGUZMAN@gmail.com>
2024-10-28 21:36:52 +0000

    cleaned up files

--- src/core/player.js
+++ src/core/player.js
@@ -1,71 +1,69 @@
 import { Assets, Sprite } from "pixi.js";
 
 export class tankPlayer {
-    constructor(playerX, playerY){
-        // setup the x, y co-ordinates
-        this.playerX = playerX;
-        this.playerY = playerY;
-        this.playerSpeed = 5;
-        this.keys = {}; // dict setup
+  constructor(playerX, playerY) {
+    // setup the x, y co-ordinates
+    this.playerX = playerX;
+    this.playerY = playerY;
+    this.playerSpeed = 5;
+    this.keys = {}; // dict setup
+  }
+
+  async initialiseSprite() {
+    // load texture of player, and convert into sprite.
+    const texture = await Assets.load('assets/images/tank.png'); // 'await' keyword used for asynchronous texture loading
+    const sprite = Sprite.from(texture);
+    sprite.anchor.set(0.5, 0.5); // set anchor point to centre of the sprite
+
+    // resizing the texture
+    const desiredWidth = 150;
+    const desiredHeight = 105;
+    sprite.scale.set(desiredWidth / texture.width, desiredHeight / texture.height); // set the scale so there is no sprite distortion
+
+    // initialise x, y to arguements passed through via constructor
+    sprite.x = this.playerX;
+    sprite.y = this.playerY;
+    this.sprite = sprite;
+  }
+
+  addToStage(app) {
+    app.stage.addChild(this.sprite);
+  }
+
+  updatePlayerPosition() {
+    // console.log(this.keys);
+    this.sprite.x = this.playerX;
+    this.sprite.y = this.playerY;
+    if (this.keys['68']) {
+      this.playerX += this.playerSpeed;
+      this.sprite.scale.x = Math.abs(this.sprite.scale.x); // make sprite face right
+    } else if (this.keys['65']) {
+      this.playerX -= this.playerSpeed;
+      this.sprite.scale.x = -Math.abs(this.sprite.scale.x); // make sprite face left
     }
+  }
 
-    async initialiseSprite(){
-        // load texture of player, and convert into sprite.
-        const texture = await Assets.load('assets/images/tank.png'); // 'await' keyword used for asynchronous texture loading
-        const sprite = Sprite.from(texture);
-        sprite.anchor.set(0.5, 0.5); // set anchor point to centre of the sprite
+  setupKeyboardControls() {
+    // 'this.keysDown.bind(this)', cannot be 'this.keysDown' as this is passed through as an event listener
+    // this therefore loses the 'this' property therefore it won't be referencing the 'tankPlayer' object
+    // this therefore causes 'this.playerX', and 'this.playerY' to be undefined 
+    window.addEventListener("keydown", this.keysDown.bind(this));
+    window.addEventListener("keyup", this.keysUp.bind(this));
+  }
 
-        // resizing the texture
-        const desiredWidth = 150;
-        const desiredHeight = 105;
-        sprite.scale.set(desiredWidth / texture.width, desiredHeight / texture.height); // set the scale so there is no sprite distortion
-
-        // initialise x, y to arguements passed through via constructor
-        sprite.x = this.playerX;
-        sprite.y = this.playerY;
-        this.sprite = sprite;
+  keysDown(e) {
+    if (e.keyCode == 68) {
+      this.keys[e.keyCode] = true;
+    } else if (e.keyCode == 65) {
+      this.keys[e.keyCode] = true;
     }
+  }
 
-    addToStage(app) {
-        app.stage.addChild(this.sprite);
+  keysUp(e) {
+    if (e.keyCode == 68) {
+      this.keys[e.keyCode] = false;
+    } else if (e.keyCode == 65) {
+      this.keys[e.keyCode] = false;
     }
-
-    updatePlayerPosition(){
-        // console.log(this.keys);
-        this.sprite.x = this.playerX;
-        this.sprite.y = this.playerY;
-        if (this.keys['68']) {
-            this.playerX += this.playerSpeed;
-            this.sprite.scale.x = Math.abs(this.sprite.scale.x); // make sprite face right
-        } else if (this.keys['65']) {
-            this.playerX -= this.playerSpeed;
-            this.sprite.scale.x = -Math.abs(this.sprite.scale.x); // make sprite face left
-        }
-    }
-
-    setupKeyboardControls() {
-        // window.addEventListener("keydown", this.keysDown);
-
-        // 'this.keysDown.bind(this)', cannot be 'this.keysDown' as this is passed through as an event listener
-        // this therefore loses the 'this' property therefore it won't be referencing the 'tankPlayer' object
-        // this therefore causes 'this.playerX', and 'this.playerY' to be undefined 
-        window.addEventListener("keydown", this.keysDown.bind(this));
-        window.addEventListener("keyup", this.keysUp.bind(this)); 
-    }
-
-    keysDown(e) {
-        if (e.keyCode == 68) {
-            this.keys[e.keyCode] = true;
-        } else if (e.keyCode == 65) {
-            this.keys[e.keyCode] = true;
-        }
-    }
-
-    keysUp(e) {
-        if (e.keyCode == 68) {
-            this.keys[e.keyCode] = false;
-        } else if (e.keyCode == 65) {
-            this.keys[e.keyCode] = false;
-        }
-    }
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,33 +1,33 @@
-import { Application, Sprite, Assets, Ticker } from "pixi.js"; // import application class
+import { Application } from "pixi.js"; // import application class
 import { tankPlayer } from "./core/player"; // import player class from js file
 import { Ground } from "./core/background.js";
 
-(async() => { // https://developer.mozilla.org/en-US/docs/Glossary/IIFE IIFE (Immediately Invoked Function Expression) JS function that runs as soon as it is defined
+(async () => { // https://developer.mozilla.org/en-US/docs/Glossary/IIFE IIFE (Immediately Invoked Function Expression) JS function that runs as soon as it is defined
 
-    // app setup 
-    const app = new Application(); // instantiating a new instance of application class
-    await app.init({ // sets up the 'canvas'; this is the area on the webpage that is controlled and managed by pixijs
-        resizeTo: window
-    });
-    app.canvas.style.position = 'absolute'; // line required in order to get rid of side bars
-    document.body.appendChild(app.canvas); // adds canvas to body
+  // app setup 
+  const app = new Application(); // instantiating a new instance of application class
+  await app.init({ // sets up the 'canvas'; this is the area on the webpage that is controlled and managed by pixijs
+    resizeTo: window
+  });
+  app.canvas.style.position = 'absolute'; // line required in order to get rid of side bars
+  document.body.appendChild(app.canvas); // adds canvas to body
 
-    // Adding ground
-    const activeGround = new Ground(app)
-    activeGround.initialiseGround();
-    app.stage.addChild(activeGround.getGround());
+  // Adding ground
+  const activeGround = new Ground(app)
+  activeGround.initialiseGround();
+  app.stage.addChild(activeGround.getGround());
 
-    // Adding player
-    const testPlayer = new tankPlayer(400, app.renderer.height - 251);
-    await testPlayer.initialiseSprite();
-    testPlayer.addToStage(app);
+  // Adding player
+  const testPlayer = new tankPlayer(400, app.renderer.height - 251);
+  await testPlayer.initialiseSprite();
+  testPlayer.addToStage(app);
 
-    testPlayer.setupKeyboardControls();
+  testPlayer.setupKeyboardControls();
 
-    // create ticker in order to update sprite positioning
-    app.ticker.add(() => {
-        testPlayer.updatePlayerPosition();
-    })
+  // create ticker in order to update sprite positioning
+  app.ticker.add(() => {
+    testPlayer.updatePlayerPosition();
+  })
 
 })();
 

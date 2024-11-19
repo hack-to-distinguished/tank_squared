5d6099a1027f96a5bfe7b2b3e81c18ae059032d8
Chris33871 <christian.harries15@gmail.com>
2024-11-19 08:10:59 +0000

    changed ground loading

--- src/core/ground.js
+++ src/core/ground.js
@@ -1,25 +1,27 @@
-import { Graphics } from "pixi.js";
+import { Graphics, GraphicsContext } from "pixi.js";
 
 export class Ground {
     constructor(app){
         this.app = app;
         this.ground = null;
-    }
+    };
+
     async initialiseGround(){
-        this.ground = new Graphics()
+        let flatGround = new GraphicsContext()
             .rect(0, this.app.renderer.height - 200, this.app.renderer.width, 200)
             .fill(0x654321);
-    }
+        this.ground = new Graphics(flatGround);
+    };
 
     getGround(){
         return this.ground;
-    }
+    };
 
     // For now this only gets the flat ground. We might have to calculate the ground at every point if we want to add hills etc
     getGroundSurface(){
         // TODO: Find a better way of getting ground lvl - Needs to work with hills
         return this.ground.getBounds().minY;
-    }
+    };
 
     // Check if anything is lower than ground and if so move it just about it
     isThereCollision(collidingSprite){
@@ -27,6 +29,6 @@ export class Ground {
         const yPosition = collidingSprite.playerY + 50;
         if (yPosition > surface){
             collidingSprite.playerY = surface - 50
-        }
-    }
+        };
+    };
 };
